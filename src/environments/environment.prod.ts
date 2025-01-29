1716dba05cac1dab5057e1e895e7747417a3709f
Adefranky <155418329+Adefranky@users.noreply.github.com>
2025-01-29 14:25:59 +0300

    test scripts and error solving

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,20 @@
+interface FirebaseConfig {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+}
+
 export const environment = {
-    production: true,
-    apiUrl: 'https://your-production-api.com/api' // Example API URL for production
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAj95Hdrka4o1aDeVILogNIPjgAxYYgdpY",
+        authDomain: "photoapp-system.firebaseapp.com",
+        projectId: "photoapp-system",
+        storageBucket: "photoapp-system.firebasestorage.app",
+        messagingSenderId: "354877554062",
+        appId: "1:354877554062:web:bba2f91509e05ccec9f8df"
+    } as FirebaseConfig
 };
