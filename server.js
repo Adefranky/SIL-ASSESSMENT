d8fa877ef11e8b38119077588c76b491ac8d2b27
Adefranky <155418329+Adefranky@users.noreply.github.com>
2025-01-29 16:15:49 +0300

    test scripts and error solving

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/photo-app'));
+app.use(express.static(__dirname + '/dist/browser'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+ '/dist/photo-app/browser/index.html'));
 });
